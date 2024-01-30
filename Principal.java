ce09306b010707145aacfcfc7c0d7f9a3e739948
AdriTomas <tomasadrian876@gmail.com>
2024-01-30 18:48:15 +0100

    Añadimos el metodo que muestra la fecha y hora actual al programa Principal

--- Principal.java
+++ Principal.java
@@ -1,5 +1,20 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class Principal{
     public static void main(String[] args){
     System.out.println("Hola mundo");
+    fechaHoraActual();
     }
+
+    public static void fechaHoraActual(){
+    Date fechaHora = new Date();
+        
+         // Formateamos fecha y hora
+        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String todoFormateado = formato.format(fechaHora);
+        
+        // Mostramos la fecha y hora formateADA
+        System.out.println("Hoy es: " + todoFormateado);
+        }
 }
